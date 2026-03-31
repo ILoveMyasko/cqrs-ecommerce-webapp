7ab8d830ac1b286ef12f0f236c30bc0ab296223f
ILOveMyasko <brutalkin_v@mail.ru>
2026-03-31 08:42:54 +0300

    comments removed

--- src/main.py
+++ src/main.py
@@ -9,13 +9,8 @@ from src.api.dependecies.db_dep import DBSessionDep
 
 @asynccontextmanager
 async def lifespan(app: FastAPI):
-    """
-    Function that handles startup and shutdown events.
-    To understand more, read https://fastapi.tiangolo.com/advanced/events/
-    """
     yield
     if sessionmanager._engine is not None:
-        # Close the DB connection
         await sessionmanager.close()
 app = FastAPI(lifespan=lifespan)
 
@@ -24,7 +19,3 @@ app.include_router(users_router)
 @app.get("/")
 async def root():
     return {"message": "Hello World"}
-
-@app.get("/hello/{name}")
-async def say_hello(name: str):
-    return {"message": f"Hello {name}"}
