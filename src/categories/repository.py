4c4983a1031d4091ec529b8469c518a52b0a6db1
myasko <komondor25@gmail.com>
2026-04-06 06:58:38 +0300

    adding categories

--- src/categories/repository.py
+++ src/categories/repository.py
@@ -1,3 +1,22 @@
 
+from uuid import UUID
+from sqlalchemy.ext.asyncio import AsyncSession
+from sqlalchemy import select
+from src.categories.models import Category
+
 class CategoryRepository:
-    def __init__(self):
+    def __init__(self, session: AsyncSession):
+        self.session = session
+
+    async def get_by_uuid(self, uuid: UUID) -> Category | None:
+        statement = select(Category).where(Category.id == uuid)
+        result = await self.session.execute(statement)
+        category = result.scalar_one_or_none()
+        return category
+
+
+    async def create(self, new_category : Category):
+        self.session.add(new_category)
+        await self.session.commit()
+        await self.session.refresh(new_category)
+        return new_category
