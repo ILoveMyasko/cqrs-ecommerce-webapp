849ea15f6dea76cb90bbf1a3f166d96fbc38acd8
ILOveMyasko <brutalkin_v@mail.ru>
2026-04-19 00:46:57 +0300

    added urls for kafka and elastic

--- src/globals/config.py
+++ src/globals/config.py
@@ -4,6 +4,8 @@ from pydantic_settings import BaseSettings, SettingsConfigDict
 class Settings(BaseSettings):
     DATABASE_URL: str
     echo_sql: bool = True
+    KAFKA_URL: str = "localhost:9092"
+    ELASTIC_URL: str = "localhost:9200"
     model_config = SettingsConfigDict(
         env_file=".env",
     )
