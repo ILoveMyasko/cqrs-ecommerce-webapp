6de28ffe9fa2732174a4e1214d7824a36887622a
ILOveMyasko <brutalkin_v@mail.ru>
2026-04-19 00:50:07 +0300

    pep

--- src/users/dependencies.py
+++ src/users/dependencies.py
@@ -6,12 +6,16 @@ from src.globals.database import DBSessionDep
 from src.users.repository import UserRepository
 from src.users.service import UserService
 
+
 def get_user_repository(db: DBSessionDep) -> UserRepository:
     return UserRepository(db)
 
+
 UserRepositoryDep = Annotated[UserRepository, Depends(get_user_repository)]
+
 
 def get_user_service(repo: UserRepositoryDep) -> UserService:
     return UserService(repository=repo)
+
 
 UserServiceDep = Annotated[UserService, Depends(get_user_service)]
